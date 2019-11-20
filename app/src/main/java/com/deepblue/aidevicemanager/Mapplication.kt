645043127254f93d0c30fff6bf85c7be3ac90d26
wug <wug@deepblueai.com>
2019-11-20 19:22:35 +0800

    wg1120

--- app/src/main/java/com/deepblue/aidevicemanager/Mapplication.kt
+++ app/src/main/java/com/deepblue/aidevicemanager/Mapplication.kt
@@ -3,21 +3,25 @@ package com.deepblue.aidevicemanager
 import android.app.Application
 import android.content.Context
 import android.support.multidex.MultiDex
+import com.baidu.mapapi.CoordType
+import com.baidu.mapapi.SDKInitializer
 import com.mdx.framework.util.CrashReportingTree
 
 import com.mdx.framework.util.Frame
 import timber.log.Timber
 
-
-/**
- * Created by bob on 2015/1/30.
- */
 class Mapplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         initTimber()
         Frame.init(applicationContext)
+        initBaiduSDK()
+    }
+
+    private fun initBaiduSDK() {
+        SDKInitializer.initialize(this)
+        SDKInitializer.setCoordType(CoordType.BD09LL)
     }
 
     private fun initTimber() {
