3f371a798e54a9d9e40a3e600a988ac6cf77f5b1
wug <wug@deepblueai.com>
2019-12-05 17:56:34 +0800

    wug20191203

--- app/src/main/java/com/deepblue/aidevicemanager/frg/FrgWDLaser.kt
+++ app/src/main/java/com/deepblue/aidevicemanager/frg/FrgWDLaser.kt
@@ -1,15 +1,11 @@
 package com.deepblue.aidevicemanager.frg
 
 import android.os.Bundle
-import com.deepblue.aidevicemanager.F
 import com.deepblue.aidevicemanager.R
-import com.deepblue.aidevicemanager.model.ModelB
+import com.deepblue.aidevicemanager.model.ModelA
 import com.deepblue.aidevicemanager.util.GlideLoader
-import com.deepblue.aidevicemanager.ws.WsStatus
 import com.google.gson.Gson
-import com.mdx.framework.util.Helper
 import kotlinx.android.synthetic.main.frg_wd_laser.*
-import kotlinx.android.synthetic.main.frg_workdetail.*
 
 class FrgWDLaser : BaseFrg() {
     override fun create(var1: Bundle?) {
@@ -29,8 +25,8 @@ class FrgWDLaser : BaseFrg() {
             1111 -> {
 //                Helper.toast("实时数据：${obj.toString()}")
                 try {
-                    F.mModelStatus?.mModelB = Gson().fromJson(obj.toString(), ModelB::class.java)
-                    GlideLoader.loadImage_error(F.mModelStatus?.mModelB?.pointYunPic, iv_wd_laser, R.mipmap.picloading, R.mipmap.picerror)
+                    val a = Gson().fromJson(obj.toString(), ModelA::class.java)
+                    GlideLoader.loadImage_error(a.pointYunPic, iv_wd_laser, R.mipmap.picloading, R.mipmap.picerror)
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
