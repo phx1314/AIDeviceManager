5b892cab7748d628729ef9d3a888a270d9dbda86
wug <wug@deepblueai.com>
2019-12-11 09:55:19 +0800

    wug20191210

--- app/src/main/java/com/deepblue/aidevicemanager/frg/FrgWDOverView.kt
+++ app/src/main/java/com/deepblue/aidevicemanager/frg/FrgWDOverView.kt
@@ -1,6 +1,5 @@
 package com.deepblue.aidevicemanager.frg
 
-import android.graphics.BitmapFactory
 import android.graphics.Point
 import android.os.Bundle
 import android.view.View
@@ -15,12 +14,12 @@ import kotlinx.android.synthetic.main.frg_wd_overview.*
 
 class FrgWDOverView : BaseFrg() {
     private val mMap by lazy { baidumap_overview.map }
-    private val mBitmapCompass by lazy {
-        BitmapFactory.decodeResource(
-            context?.resources,
-            R.drawable.u2262
-        )
-    }
+//    private val mBitmapCompass by lazy {
+//        BitmapFactory.decodeResource(
+//            context?.resources,
+//            R.drawable.u2262
+//        )
+//    }
 
     override fun create(var1: Bundle?) {
         setContentView(R.layout.frg_wd_overview)
@@ -29,7 +28,7 @@ class FrgWDOverView : BaseFrg() {
     override fun initView() {
         iv_overview_zoom.setOnClickListener(this)
         val builder = MapStatus.Builder()
-        builder.zoom(15.0f).target(LatLng(31.8233, 120.015008)).overlook(-10F)
+        builder.zoom(15.0f).target(LatLng(31.219703, 121.391131)).overlook(-10F)
         mMap.setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()))
 
         mMap.mapType = BaiduMap.MAP_TYPE_NORMAL //地图普通
@@ -44,7 +43,7 @@ class FrgWDOverView : BaseFrg() {
         )
 
         mMap.compassPosition = Point(50, 50) //指南针位置
-        mMap.setCompassIcon(mBitmapCompass)  //自定义指南针图标
+//        mMap.setCompassIcon(mBitmapCompass)  //自定义指南针图标
         baidumap_overview.logoPosition = LogoPosition.logoPostionRightBottom //logo位置
         baidumap_overview.showScaleControl(false) //比例尺按钮隐藏
         baidumap_overview.showZoomControls(false) //缩放按钮隐藏
@@ -72,7 +71,7 @@ class FrgWDOverView : BaseFrg() {
 
                     val locData = MyLocationData.Builder()
                         .accuracy(0F)
-                        .direction(0F)
+                        .direction(F.mModelStatus?.mModelB?.data_yaw_angle?.toFloat()!!)
                         .latitude(mCleanPealPosition.lati).longitude(mCleanPealPosition.longti).build()
                     mMap.setMyLocationData(locData)
                 } catch (e: Exception) {
