6a4f07036f1f3b03a5dfcca4525e3b747a3173cc
wug <wug@deepblueai.com>
2019-12-25 10:04:51 +0800

    20191225 实体类名修改

---
+++ app/src/main/java/com/deepblue/aidevicemanager/model/ModelTime.kt
@@ -0,0 +1,17 @@
+package com.deepblue.aidevicemanager.model
+
+class ModelTime {
+
+
+    /**
+     * type : 1
+     * number :
+     * time : 12:08
+     * showValue : 每天
+     */
+
+    var type: Int = 0
+    var number: String? = null
+    var time: String? = null
+    var showValue: String? = null
+}
